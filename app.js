5f4cdec99ef8dc3f7e69222dd9bb7f26c7fb7a4a
ma3tk <masataka0227@gmail.com>
2017-11-04 17:07:00 +0900

    Added dotenv

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
+require('dotenv').config();
 const express = require('express')
 const middleware = require('@line/bot-sdk').middleware
 
 const app = express()
 
 const config = {
-  channelAccessToken: 'YOUR_CHANNEL_ACCESS_TOKEN',
-    channelSecret: 'YOUR_CHANNEL_SECRET'
-    }
+    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+    channelSecret: process.env.CHANNEL_SECRET
+}
 
 app.post('/webhook', middleware(config), (req, res) => {
   res.json(req.body.events) // req.body will be webhook event object
-  })
+})
 
 app.listen(8080)
